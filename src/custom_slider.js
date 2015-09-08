ef854db2ed2428c48842fcabe9f4413c21dfdf0a
Zakharov Ivan <chronoiicross@gmail.com>
2015-09-08 16:22:15 +0900

    Beta Fivetronics

--- src/custom_slider.js
+++ src/custom_slider.js
@@ -3,13 +3,27 @@ $(document).ready(function() {
     loop:true,
     items: 1,
     nav: true,
-    animateOut: "slideInRight",
-    dots: true
+    navSpeed: 800,
+    navText: ["",""]
+    // animateOut: "slideInRight"
   });
 
   $(".owl-item").css("height", $(window).height());
   $(window).resize(function(){
     $(".owl-item").css("height", $(window).height());
+  });
+  $(".owl-next").addClass("fa fa-angle-right fa-5x");
+  $(".owl-prev").addClass("fa fa-angle-left fa-5x");
+
+  $(".owl-next, .owl-prev").click(function(){
+    if ($(".active").children(".customItem").hasClass("direct")){
+      $(".fa-angle-left, .fa-angle-right").addClass("inverse");
+      $("#baz").attr("src","/images/logo-in.png");
+    }
+    else{
+      $(".fa-angle-left, .fa-angle-right").removeClass("inverse");
+      $("#baz").attr("src","/images/logo.png");
+    }
   });
   // var owl = $('.owl-carousel');
   // owl.on('prev.owl.carousel', function(event){
--- src/hover.js
+++ src/hover.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+  // $("div").css("opacity","1");
+
+  $(".image").ready(function(){
+
+    // $(this).css("opacity", "0.7");
+  });
 
   if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     $("#arrow").addClass("vertical");
@@ -7,6 +13,29 @@ $(document).ready(function(){
   {
     $("#arrow").addClass("horizontal");
   }
+
+
+  $("#right > .intro").hover(
+    function(){
+      $("#right > .image").css("transform", "scale(1.07,1.07)");
+    },
+    function(){
+      $("#right > .image").css("transform", "scale(1.01,1.01)");
+    }
+  );
+
+ $("#left > .intro").hover(
+    function(){
+      $("#left > .image").css("transform", "scale(1.07,1.07)");
+    },
+    function(){
+      $("#left > .image").css("transform", "scale(1.01,1.01)");
+    }
+  );
+
+
+
+
 
   $("#arrow").click(function(){
     if ($("#arrow").hasClass("horizontal")) {
